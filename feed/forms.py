c945727b91d73fc53a48755fe46f6a932d7a14ec
Koboju Kibeyefa <kobojuezekiel@gmail.com>
2022-05-04 03:27:27 +0100

    Added files

---
+++ feed/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Comments, Post
+
+
+class NewPostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ['description', 'tags',  'pic']
+
+
+class NewCommentForm(forms.ModelForm):
+
+	class Meta:
+		model = Comments
+		fields = ['comment']
